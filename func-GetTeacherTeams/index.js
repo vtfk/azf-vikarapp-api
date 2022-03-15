26d1b24ace443d7862fb145c052845347816a175
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2022-03-15 12:41:01 +0100

    Lagt til rettighetsstyring på hvilke teams som kan søkes opp basert på lokasjon

--- func-GetTeacherTeams/index.js
+++ func-GetTeacherTeams/index.js
@@ -1,18 +1,36 @@
 const { azfHandleResponse, azfHandleError } = require('@vtfk/responsehandlers')
 const { prepareRequest } = require('../lib/_helpers')
-const { getOwnedObjects } = require('../lib/msgraph')
+const { getUser, getOwnedObjects } = require('../lib/msgraph')
+const { getPermittedLocations } = require('../lib/getPermittedLocations')
+const HTTPError = require('../lib/httperror')
 
 module.exports = async function (context, req) {
   try {
     // Prepare the request
     const required = ['params.upn']
-    await prepareRequest(req, { required })
+    const { requestor } = await prepareRequest(req, { required })
+
+    // If the user is not admin, make sure that the user has permissing to see the users teams
+    if(!requestor.roles.includes('App.Admin')) {
+      // Get the user to get teams from
+      const user = await getUser(req.params.upn);
+      if(!user) throw new Error(`Could not find a user with upn ${req.params.upn}`);
+
+      // Get the requestors permitted locations
+      const permittedLocations = await getPermittedLocations(requestor);
+      const permittedNames = permittedLocations.map((i) => i.name);
+
+      // Throw if the user is not in the permitted locations
+      if(!permittedNames.includes(user.officeLocation)) throw new HTTPError(401, `Du har ikke lov til å se teams for lokasjon '${user.officeLocation}'`, 'Manglende rettigheter')
+    }
 
     // Retreive the owned objects
     let data = await getOwnedObjects(req.params.upn)
 
     // Filter out any resources that is not an SDS team
-    data = data.filter((i) => i.mail && i.mail.toLowerCase().startsWith('section_'))
+    data = data.filter((i) => i.mail && i.mail.toLowerCase().startsWith('section_')) 
+    // Filter out any resources that are expired
+    data = data.filter((i) => !i.displayName.toLowerCase().startsWith('exp'))
 
     // Send the response
     return await azfHandleResponse(data, context, req)
