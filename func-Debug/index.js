45eec7421e18d84105cd40479ea8ced7d9c9982e
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2022-04-01 14:00:37 +0200

    Standard

--- func-Debug/index.js
+++ func-Debug/index.js
@@ -1,6 +1,6 @@
 const { azfHandleResponse, azfHandleError } = require('@vtfk/responsehandlers')
 
-const { getGroupMembers, getGroupOwners, addGroupOwner, removeGroupOwner } = require('../lib/msgraph')
+// const { getGroupMembers, getGroupOwners, addGroupOwner, removeGroupOwner } = require('../lib/msgraph')
 
 module.exports = async function (context, req) {
   try {
@@ -14,8 +14,8 @@ module.exports = async function (context, req) {
     // const data = await db.Schools.findByIdAndUpdate(req.params.id, req.body, { new: true })
 
     // const data = await addTeamsMembership('8df65c9e-a0a4-4599-872c-2d3ba654556b', 'ae34b275-ee14-4ece-b4a0-a03e7dc96313');
-    let data = {}
-    data = await getGroupOwners('5735b78c-2f25-40a8-89e2-29c587b1032c')
+    const data = {}
+    // data = await getGroupOwners('5735b78c-2f25-40a8-89e2-29c587b1032c')
     // data = await addGroupOwner('5735b78c-2f25-40a8-89e2-29c587b1032c', 'ae34b275-ee14-4ece-b4a0-a03e7dc96313')
     // data = await removeGroupOwner('5735b78c-2f25-40a8-89e2-29c587b1032c', 'ae34b275-ee14-4ece-b4a0-a03e7dc96313')
 
