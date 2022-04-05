67dff0811a8a3b815ea468f30f4ab5a358108461
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2022-04-05 12:47:30 +0200

    Implementet logs endpoint

---
+++ func-GetLogs/index.js
@@ -0,0 +1,22 @@
+const config = require('../config')
+const { azfHandleResponse, azfHandleError } = require('@vtfk/responsehandlers')
+const { prepareRequest } = require('../lib/_helpers')
+const { logToDB } = require('../lib/common')
+const { Logs } = require('../lib/db')
+
+module.exports = async function (context, req) {
+  let requestor
+  try {
+    // Prepare the request
+    ;({ requestor } = await prepareRequest(req))
+    // if (!requestor.roles.includes('App.Admin')) throw new HTTPError(401, 'Du har ikke rettigheter til å gjennomføre denne handlingen')
+
+    const data = await Logs.find();
+
+    // Send the response
+    return await azfHandleResponse(data, context, req)
+  } catch (err) {
+    logToDB('error', err, req, context, requestor)
+    return await azfHandleError(err, context, req)
+  }
+}
