6eb9a8d4f0a925dbdd7f67c17f31fa9b4eb08e15
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2022-04-07 13:34:06 +0200

    Fjernet log tester

--- func-GetLogs/index.js
+++ func-GetLogs/index.js
@@ -4,18 +4,13 @@ const { logToDB } = require('../lib/common')
 const { Logs } = require('../lib/db')
 
 module.exports = async function (context, req) {
-  context.log('Application Insights log test')
   let requestor
   try {
     // Prepare the request
     ;({ requestor } = await prepareRequest(req))
-    // if (!requestor.roles.includes('App.Admin')) throw new HTTPError(401, 'Du har ikke rettigheter til å gjennomføre denne handlingen')
+    if (process.env.NODE_ENV !== 'development' && !requestor.roles.includes('App.Admin')) throw new HTTPError(401, 'Du har ikke rettigheter til å gjennomføre denne handlingen')
 
-    context.log('Retreiving logs')
     const data = await Logs.find({})
-
-    context.log('Logs')
-    context.log(data)
 
     // Send the response
     return await azfHandleResponse(data, context, req)
